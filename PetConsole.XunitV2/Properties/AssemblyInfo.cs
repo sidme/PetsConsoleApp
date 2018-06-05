7850acca14390feabb7dd4c9043e8aad5bf31545
Sid Kumar <sidme.au@gmail.com>
2018-06-05 21:16:39 +1000

    Project Added

---
+++ PetConsole.XunitV2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PetConsole.XunitV2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PetConsole.XunitV2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("566b67b5-1858-428b-91d2-02b2b5ce474b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PetConsole.XunitV2/UnitTestApiClient.cs
@@ -0,0 +1,38 @@
+﻿using NSubstitute;
+using PetsConsoleApp.API;
+using PetsConsoleApp.DTO;
+using System.Collections.Generic;
+using System.Configuration;
+using Xunit;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace PetConsole.XunitV2
+{
+    /// <summary>
+    /// Set of unit test to test funcationality of ApiClient abstract class by mocking.
+    /// </summary>
+    public class UnitTestApiClient
+    {
+        /// <summary>
+        /// Test Case 1: Testing what happens when ApiClient.Get() returns null to consumer.
+        /// Expected Output: If null, test pass
+        /// </summary>
+        [Fact]
+        public void XApiClient_Get_ReturnNull_Pass()
+        {
+            //Arrange
+            var abstractObject = Substitute.For<ApiClient>();
+            var apiUrl = ConfigurationManager.AppSettings["ApiUrl"];
+            abstractObject.Get<List<PetOwner>>(apiUrl);
+            //var abstractObject = MockRepository.GeneratePartialMock<ApiClient>();
+            //abstractObject.Stub(x => x.Get<List<PetOwner>>(ConfigurationManager.AppSettings["ApiUrl"])).Return(null);
+
+            //Act
+            var result = abstractObject.Get<List<PetOwner>>(ConfigurationManager.AppSettings["ApiUrl"]);
+
+            //Assert
+            Assert.IsNull(result);
+
+        }
+    }
+}

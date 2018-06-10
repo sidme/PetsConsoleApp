118bd54abb874e363ab739d73628503b953a9d5c
Sid Kumar <sidme.au@gmail.com>
2018-06-10 18:58:23 +1000

    Unit Test Changes 01

--- PetConsole.XunitV2/UnitTestApiClient.cs
+++ PetConsole.XunitV2/UnitTestApiClient.cs
@@ -1,6 +1,7 @@
 ﻿using NSubstitute;
 using PetsConsoleApp.API;
 using PetsConsoleApp.DTO;
+using PetsConsoleApp.Model.Enum;
 using System.Collections.Generic;
 using System.Configuration;
 using Xunit;
@@ -24,9 +25,7 @@ namespace PetConsole.XunitV2
             var abstractObject = Substitute.For<ApiClient>();
             var apiUrl = ConfigurationManager.AppSettings["ApiUrl"];
             abstractObject.Get<List<PetOwner>>(apiUrl);
-            //var abstractObject = MockRepository.GeneratePartialMock<ApiClient>();
-            //abstractObject.Stub(x => x.Get<List<PetOwner>>(ConfigurationManager.AppSettings["ApiUrl"])).Return(null);
-
+            
             //Act
             var result = abstractObject.Get<List<PetOwner>>(ConfigurationManager.AppSettings["ApiUrl"]);
 
@@ -34,5 +33,91 @@ namespace PetConsole.XunitV2
             Assert.IsNull(result);
 
         }
+
+        /// <summary>
+        /// Test Case 3: Testing to see what happens when ApiClient.Get() returns one object of a type
+        /// Expected Output: Test passes if list contains one element.
+        /// </summary>
+        [Fact]
+        public void XApiClient_Get_ReturnOneObject_Pass()
+        {
+            //Arrange
+            var abstractObject = Substitute.For<ApiClient>();
+            var apiUrl = ConfigurationManager.AppSettings["ApiUrl"];
+
+            var expectedResult = new List<PetOwner>
+            {
+                new PetOwner
+                {
+                    Age = 20,
+                    Gender = Gender.Female,
+                    Name = "Name 1",
+                    Pets = new List<Pet> {new Pet {Name = "Cat 1", Type = PetType.Cat}}
+                }
+            };
+
+            //Act
+            abstractObject.Get<List<PetOwner>>(apiUrl).Returns(expectedResult);
+
+            //Assert
+            Assert.IsTrue(true);
+
+        }
+
+        /// <summary>
+        /// Test Case 4: Testing to see if ApiClient.Get() returns one element with null Pets
+        /// Expected Output: Test passes if pets object is null
+        /// </summary>
+        //[Fact]
+        //public void ApiClient_Get_ReturnNullPetsWithOneOwner_Pass()
+        //{
+        //    //Arrange
+        //    var abstractObject = MockRepository.GeneratePartialMock<ApiClient>();
+        //    abstractObject.Stub(x => x.Get<List<PetOwner>>(ConfigurationManager.AppSettings["ApiUrl"])).Return(
+        //        new List<PetOwner>
+        //        {
+        //            new PetOwner
+        //            {
+        //                Age = 20,
+        //                Gender = Gender.Female,
+        //                Name = "Name 1",
+        //                Pets = null
+        //            }
+        //        });
+
+        //    //Act
+        //    var result = abstractObject.Get<List<PetOwner>>(ConfigurationManager.AppSettings["ApiUrl"]);
+
+        //    //Assert
+        //    Assert.IsNull(result[0].Pets);
+
+        //}
+
+        /// <summary>
+        /// Test Case 5: Test to see any exception is thrown by abstract class.
+        /// Expected Output: If Exception is thrown, test fails.
+        /// Additional Comments: A better approach is to test for specific exceptions like TimeoutException, JsonSerialisationException etc.
+        /// </summary>
+        //[Fact]
+        //public void ApiClient_Get_AnyExceptionThrownByAPiClient_FailIfThrown()
+        //{
+        //    //Arrange
+        //    var abstractObject = MockRepository.GeneratePartialMock<ApiClient>();
+        //    abstractObject.Stub(x => x.Get<List<PetOwner>>(ConfigurationManager.AppSettings["ApiUrl"])).Return(null);
+
+
+        //    try
+        //    {
+        //        //Act
+        //        var result = abstractObject.Get<List<PetOwner>>(ConfigurationManager.AppSettings["ApiUrl"]);
+
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        //Assert
+        //        Assert.IsTrue(exception != null);
+        //    }
+        //}
+
     }
 }
